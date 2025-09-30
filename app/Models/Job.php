9d3760696c5d88883a666ca3380c0a0a5fab4f15
Labici Ioan <ioanlabici@gmail.com>
2025-09-30 07:06:08 +0300

    lw-1: Tag and Job models and DB relationship

---
+++ app/Models/Job.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Job extends Model
+{
+    /** @use HasFactory<\Database\Factories\JobFactory> */
+    use HasFactory;
+
+    protected $table = 'jobs_list';
+
+    protected $fillable = [
+        'title',
+        'salary',
+        'employer_id',
+    ];
+
+    public function employer(): BelongsTo {
+        return $this->belongsTo(Employer::class);
+    }
+
+    public function tags(): BelongsToMany {
+        return $this->belongsToMany(Tag::class);
+    }
+}
