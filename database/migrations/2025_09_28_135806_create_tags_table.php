f1e05d2214baab21da86e8fb6b604e00fd869cd6
Labici Ioan <ioanlabici@gmail.com>
2025-10-02 07:16:58 +0300

    lw-1: Database migration files.

---
+++ database/migrations/2025_09_28_135806_create_tags_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Models\Job;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tags', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+        });
+
+        // Pivot table
+        Schema::create('job_tag', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Job::class, 'job_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('tag_id')->constrained()->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tags');
+        Schema::dropIfExists('job_tag');
+    }
+};
