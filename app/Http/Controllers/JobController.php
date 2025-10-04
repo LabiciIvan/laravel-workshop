56e91310aa825348ec9b56dd340a356f542f976d
Labici Ioan <ioanlabici@gmail.com>
2025-10-04 10:57:05 +0300

    lw-1: Job controller.

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -3,9 +3,42 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Employer;
+use App\Models\Job;
 use Illuminate\Http\Request;
 
-class JobController extends Controller
-{
-    //
+class JobController extends Controller {
+
+    public function index() {
+        $jobs = Job::with('employer')->latest()->paginate(5);
+
+        return view('jobs.jobs', ['jobs' => $jobs]);
+    }
+
+    public function show(int $id) {
+        $job = Job::with('employer')->findOrFail($id);
+        return view('jobs.show', ['job' => $job]);
+    }
+
+    public function store(Request $request) {
+        $data = [
+            'title'         => $request->input('name'),
+            'salary'        => $request->input('salary'),
+            'employer_id'   => $request->input('employer')
+        ];
+
+        $tags = [1, 2];
+
+        $job = Job::create($data);
+
+        $job->tags()->attach($tags);
+
+        return view('jobs.show', ['job' => $job]);
+    }
+
+    public function create() {
+        $employers = Employer::select(['id', 'name'])->get();
+        return view('jobs.create', ['employers' => $employers]);
+    }
+
 }
